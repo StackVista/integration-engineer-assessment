b186c746eb811d4a2018bda7f6df368255cac1a7
Zandré Witte <zwitte@stackstate.com>
2022-02-04 11:59:33 +0100

    update readme

--- weather-integration.py
+++ weather-integration.py
@@ -3,12 +3,6 @@ from stackstate_checks.base import AgentCheck, AgentIntegrationInstance, Configu
 
 class WeatherIntegration(AgentCheck):
 
-    def get_instance_key(self, instance):
-        if 'city' not in instance:
-            raise ConfigurationError('Missing city in topology instance configuration.')
-
-        return AgentIntegrationInstance("weather", instance['city'])
-
     def check(self, instance):
         # implement check code
         pass
